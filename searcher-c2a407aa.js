db302d17e9c76ec46bfb86e0c71f8f3a8848913d
Biao Jiang <biaohz@gmail.com>
2026-01-01 14:34:51 +0100

    Deploy site on 2026-01-01

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-393c426d.js',
+                path_to_root + 'searchindex-dcafd18e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
